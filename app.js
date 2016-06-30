36d7902178b86b6fa331f1b8375ebfe07660c8c2
Yao Lu <ylu31@ncsu.edu>
2016-06-29 22:41:45 -0700

    Finished Chapter 3

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ app.controller('GalleryController', function(){
     };
   });
 
+app.controller('ReviewController', function(){
+    this.review = {};
+    this.addReview = function(product) {
+      this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
  var gems = [{
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
